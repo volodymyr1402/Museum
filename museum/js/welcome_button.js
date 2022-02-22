000aa06d8a4bc3eb572875b325b69b8e81bd7013
Volodymyr_Kolyada <90142111+volodymyr1402@users.noreply.github.com>
2022-02-22 16:01:17 +0200

    modify welcome button

--- museum/js/welcome_button.js
+++ museum/js/welcome_button.js
@@ -1,1 +1,5 @@
-
+const button = document.querySelector('.welcome_button')
+button.addEventListener('click' , (event)=>{
+    event.preventDefault()
+    window.open('https://www.google.com/maps/@48.8618159,2.3366818,3a,75y,81.55h,73.4t/data=!3m7!1e1!3m5!1sAF1QipOVxZQuSy3Bx9T_HpH_7FtBHDTXvI6SF-A10ocT!2e10!3e12!7i5472!8i2736')
+})
